374ceb0ced15ae1930ad076419214f2e6e7788a5
FacundoDavidFlores <117491931+FacundoDavidFlores@users.noreply.github.com>
2024-09-01 23:33:02 -0300

    Mejoré un poco el StopMotion
    Eso.

--- Scripts/Player_Script.gd
+++ Scripts/Player_Script.gd
@@ -72,7 +72,7 @@ func _ready() -> void:
 	pass
 #-------------------------------------------------------------------------------
 func _physics_process(_delta:float) -> void:
-	deltaTimeScale = _delta * 60
+	deltaTimeScale = Engine.time_scale
 	movementInput = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down")).normalized()
 	camera_2d.position = lerp(camera_2d.position, cameraMarker.global_position, 0.1 * deltaTimeScale)
 	leftWallCol = leftWall_Area2D.get_overlapping_bodies()
